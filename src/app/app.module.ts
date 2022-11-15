53c025c3f6248f385b6626e963dfa865ef55a218
DNL0815 <dimitri.luck@outlook.com>
2022-11-14 21:27:36 -0500

    p.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
